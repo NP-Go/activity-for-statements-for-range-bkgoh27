91b8d9d02f48c46e3d995555916df4b29f22b341
bkgoh27 <bengkoon.goh27@gmail.com>
2022-03-29 14:23:18 +0800

    ask for two input numbers

--- askForRangeLoop.go
+++ askForRangeLoop.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
-	//Insert code for only even here
+	var input1, input2 int
+	fmt.Println("Enter first number: ")
+	fmt.Scanln(&input1)
+	fmt.Println("Enter second number: ")
+	fmt.Scanln(&input2)
+
+	for i := input1; i <= input2; i++ {
+		if i%2 == 0 {
+			fmt.Println("Even")
+		} else {
+			fmt.Println("Odd")
+		}
+		fmt.Println(i)
+	}
+	fmt.Println("Done!")
 }
